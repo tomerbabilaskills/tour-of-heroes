5f0e502f15ee2a5d68f463ec5e7554f06d14b127
Tomer Babila <tomerb@academy-skills.com>
2021-06-29 17:00:28 +0300

    Fix a bug in messages component and add style to messages

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,10 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -19,10 +23,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Select hero id=${hero.id}`);
   }
 
   getHeroes(): void {
-    // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
