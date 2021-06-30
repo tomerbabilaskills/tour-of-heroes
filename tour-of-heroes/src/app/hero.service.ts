17c4794316e7355851003b40c0309368ca5900b2
Tomer Babila <tomerb@academy-skills.com>
2021-06-30 11:17:24 +0300

    Add new hero

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -38,6 +38,13 @@ export class HeroService {
     return this.http.put(this.heroesUrl, hero, this.httpOptions);
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
